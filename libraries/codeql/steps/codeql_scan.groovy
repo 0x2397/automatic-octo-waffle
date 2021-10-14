ebd9df69c46e8ed5925b5c4d12f694974310ae0f
0x38F <0x38F@local>
2021-10-14 12:06:19 -0500

    add global configuration

--- libraries/codeql/steps/codeql_scan.groovy
+++ libraries/codeql/steps/codeql_scan.groovy
@@ -12,8 +12,8 @@ void call() {
     echo "Running CodeQL scans"
     sh "curl -LO https://github.com/github/codeql-action/releases/latest/download/codeql-bundle-linux64.tar.gz"
     sh "tar -xvzf ./codeql-bundle-linux64.tar.gz"
-    sh "codeql/codeql database create codeql-db --language=${codeql_config["merged"].language}"
-    sh "codeql/codeql database analyze codeql-db ${codeql_config["merged"].language}-security-and-quality.qls --format=sarifv2.1.0 --output=analysis-results.sarif"
+    sh "codeql/codeql database create codeql-db --language=${codeql_config.merged.language}"
+    sh "codeql/codeql database analyze codeql-db ${codeql_config.merged.language}-security-and-quality.qls --format=sarifv2.1.0 --output=analysis-results.sarif"
     sh "codeql/codeql github upload-results --repository=${repo_name} --ref=refs/heads/${BRANCH_NAME} --commit=${GIT_COMMIT} --sarif=analysis-results.sarif"
-    sh "curl --user ${codeql_config["merged"].creds} -o alerts.json https://api.github.com/repos/${repo_name}/code-scanning/alerts?ref=refs/heads/${BRANCH_NAME}&state=open&per_page=100"
+    sh "curl --user ${codeql_config.merged.creds} -o alerts.json https://api.github.com/repos/${repo_name}/code-scanning/alerts?ref=refs/heads/${BRANCH_NAME}&state=open&per_page=100"
 }
